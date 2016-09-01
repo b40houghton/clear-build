d5f23c76c1b839a3c8a913e35ad3df96c392f39f
Brandon Houghton <40.houghton@gmail.com>
2016-09-01 17:15:38 -0500

    Add initial files

---
+++ index.js
@@ -0,0 +1,17 @@
+var rmdir = require('rimraf');
+var buildDirectoryGlob = './build';
+var buildFileGlob = './*(build)*.zip';
+
+function removeBuildDirectory() {
+	rmdir(buildDirectoryGlob, function () {
+		console.log('Build removed');
+	});
+}
+
+function removeBuildFile(callback) {
+	rmdir(buildFileGlob, function () {
+		return callback();
+	});
+}
+
+removeBuildFile(removeBuildDirectory);
